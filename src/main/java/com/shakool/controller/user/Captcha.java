3578a7d4235d2c802cad61d044706a439c8d2dea
geekgao <936302529@qq.com>
2016-03-29 19:03:58 +0800

    改变项目结构

--- src/main/java/com/shakool/controller/user/Captcha.java
+++ src/main/java/com/shakool/controller/user/Captcha.java
@@ -1,4 +1,4 @@
-package com.shakool.controller;
+package com.shakool.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
